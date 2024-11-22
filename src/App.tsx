e56efa9abdd7a37a1acbfed27d46ea59acf2c7cd
Maria Starostina <maria.starostina.work@gmail.com>
2024-11-22 22:49:17 +0300

    Update npm, add template paths, create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 function App() {
   return (
-    <div className="font-sans">
-      <h1 className="text-4xl">Test</h1>
+    <div className="font-sans bg-[#f1d4b3] min-h-screen">
+      <h1 className="text-[120px] font-bold text-black/5 uppercase absolute left-1/2 -translate-x-1/2">Todo list</h1> 
+      <main>
+        <header></header>
+        <ul></ul>
+        <section></section>
+      </main>
+      <footer></footer>
     </div>
-  ) ;
+  );
 }
 
 export default App
