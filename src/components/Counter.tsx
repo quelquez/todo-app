8f471ca673563f7d3f0495707dd6a66cd91e0a1b
Maria Starostina <maria.starostina.work@gmail.com>
2024-11-25 05:31:43 +0300

    Create Logo, Counter components

---
+++ src/components/Counter.tsx
@@ -0,0 +1,7 @@
+export default function Counter() {
+  return (
+    <p>
+      <b>0</b> / 0 todos completed
+    </p>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
+import Counter from "./Counter"
+import Logo from "./Logo"
+
 export default function Header() {
   return (
-    <header className="col-[1/3] row-[1/2] bg-[#ece0d1] border-b border-[#38220f]/[0.08]"></header>
+    <header className="flex justify-between items-center px-[28px] col-[1/3] row-[1/2] bg-[#ece0d1] border-b border-[#38220f]/[0.08]">
+        <Logo />
+        <Counter />
+    </header>
   )
 }
