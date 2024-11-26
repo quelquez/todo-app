0b24be1ad5ed58a326e2622212ca02ab5b9e8290
Maria Starostina <maria.starostina.work@gmail.com>
2024-11-26 04:07:59 +0300

    Add delete button component, styling (line-through), add "logo"

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 export default function Logo() {
   return (
-    <img src="" /> 
+    <div>☕</div>
   )
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,31 @@
+import DeleteButton from "./DeleteButton";
+
+const initialTodos = [
+  {
+    text: "Example1",
+    isCompleted:false,
+  },
+  {
+    text: "Example2",
+    isCompleted:true,
+  },
+  {
+    text: "Example3",
+    isCompleted:false,
+  },
+];
 export default function TodoList() {
   return (
-    <ul></ul>
-  )
+    <ul>
+      {initialTodos.map((todo) => (
+        <li
+          key={todo.text}
+          className="flex justify-between items-center px-8 h-[80px] text-[18px] cursor-pointer border-b border-black/[8%]"
+        >
+          <span className={`${todo.isCompleted ? 'line-through text-[#967259]' : " "} `}>{todo.text } </span>
+          <DeleteButton />
+        </li>
+      ))}
+    </ul>
+  );
 }
